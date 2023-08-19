cab3db7ee6b1ab1afe7d310b1b0a2ee21df74382
Pedro Pisandelli <pisandelli@gmail.com>
2023-08-19 15:01:29 -0300

    Footer

--- components/widgets/Footer/Footer.vue
+++ components/widgets/Footer/Footer.vue
@@ -7,26 +7,127 @@
  * @example
  * <WFooter></WFooter>
  */
+
+const footerMenu = [
+  {
+    label: 'Todos os cursos',
+    url: '#'
+  },
+  {
+    label: 'Dúvidas frequentes',
+    url: '#'
+  },
+  {
+    label: 'Suporte Premium',
+    url: '#'
+  },
+  {
+    label: 'Carreiras',
+    url: '#'
+  },
+  {
+    label: 'Cursos em destaque',
+    url: '#'
+  },
+  {
+    label: 'Seja um instrutor',
+    url: '#'
+  },
+  {
+    label: 'Planos e preços',
+    url: '#'
+  },
+  {
+    label: 'Política de privacidade',
+    url: '#'
+  },
+  {
+    label: 'Trilha Backend',
+    url: '#'
+  },
+  {
+    label: 'Trilha Frontend',
+    url: '#'
+  },
+  {
+    label: 'Trilha Fullstack ',
+    url: '#'
+  },
+  {
+    label: 'Trilha Gestão de TI',
+    url: '#'
+  }
+]
+
+const now = useDateFormat(useNow(), 'YYYY')
 </script>
 
 <template lang="pug">
 footer.footer
   StackL
-    .header
-      ClusterL
-        CLogo.logo
-        CCompanies.companies
+    BoxL(borderless transparent)
+      StackL(wide)
+        .header
+            ClusterL(between)
+              CLogo.logo
+              ClusterL(narrow)
+                h3 Nós treinamos
+                CCompanies.companies
+        .body
+          ClusterL(tag='section' between)
+            CNavigation.footer-menu(:stack='true' :data='footerMenu')
+            .gap
+              CenterL
+                StackL.info
+                  p Aenean eleifend ac sapien ac mollis. Vestibulum in lectus ante. Nunc fringilla, ex nec viverra posuere, nibh nulla euismod justo, in semper tellus purus non ipsum.
+                  ClusterL
+                    CButton.cta(tag='a' to='/' accent pill) Matricule-se agora
+                    p(class='text:bold') São mais de 700 cursos e seminários
+        .disclaimer
+          ClusterL(between)
+            p &copy; Todos os direitos reservados - {{ now }}
+            ClusterL
+              NuxtLink(to='#') Politica de privacidade
+              NuxtLink(to='#') Termos de uso
 
 </template>
 
 <style lang="stylus" scoped>
 .footer
-  padding-inline: var(--gap)
+  margin-block-start: calc(var(--gap) * 2)
+  padding: var(--gap)
   background-color: var(--color-primary)
+
+  & * + *
+    color: var(--color-neutral)
 
 .logo
   inline-size: 8rem
 
 .companies
   --color-symbol: var(--color-neutral)
+
+.footer-menu
+  display: block
+  height: 9rem
+  columns: 2
+  column-gap: var(--s4)
+
+:deep(.anchor)
+  text-decoration: none
+
+  &:is(:hover, :focus)
+    text-decoration: underline
+
+.disclaimer
+  padding-block-start: var(--gap)
+  border-top: 1px solid #FFFFFF40
+
+.gap
+  --max-width: 75ch
+  inline-size: 50%
+.info
+  font-size: var(--font-size-big)
+  font-weight: var(--weight-light)
+  max-inline-size: var(--max-text-width)
 </style>
---
+++ components/widgets/MainNav/MainNav.vue
@@ -0,0 +1,81 @@
+<script lang="ts" setup>
+/**
+ * Render Horizontal or Vertical Navigation.
+ * @name 'c-navigation'
+ * @alias 'CNavigation'
+ * @version 1.0.0
+ * @example
+ * <CNavigation></CNavigation>
+ */
+
+// Main navigation data
+import type { NavigationTypes } from 'interfaces/NavigationTypes'
+
+defineProps<{
+  data: NavigationTypes[] | undefined
+  stack: boolean
+}>()
+
+// Get current Viewport
+const viewport = useViewport()
+
+// Toggle Menu visibility
+const showMenu = ref(false)
+function toggleMenu() {
+  showMenu.value = !showMenu.value
+}
+
+// Apply classes to menu based on current viewport and visibility
+const checkVisibility = computed(() => {
+  const visible = showMenu.value || viewport.isGreaterOrEquals('desktop')
+  const isVisible = !visible ? 'display:none' : ''
+  const isBox = viewport.isLessThan('desktop') ? 'inBox' : ''
+
+  return `${isBox} ${isVisible}`
+})
+</script>
+
+<template lang="pug">
+nav
+  ClusterL.main-nav(:class="checkVisibility")
+    //- CNavigation(:data='data')
+    //- button.menuIcon(v-if="viewport.isLessThan('desktop')" to="#" icon-only icon="ion:menu" aria-hidden="true" @click='toggleMenu()')
+</template>
+
+<style lang="stylus" scoped>
+.main-nav
+  &.inBox
+    min-inline-size: 15rem
+    border-radius: var(--border-radius)
+    position: absolute
+    padding: var(--gap)
+    top: 80%
+    right: @padding
+    background-color: var(--color-neutral)
+    _box-shadow()
+
+    .menu
+      .item
+        margin-bottom: var(--gap)
+
+.item
+  text-transform: uppercase
+  font-weight: var(--weight-bold)
+
+  a
+    color: var(--navigation-item__color, var(--color-neutral, #fbfbfb))
+    background-color: var(--navigation-item__bg, transparent)
+    text-decoration: none
+    padding-inline: var(--s0)
+    padding-block: var(--s-1)
+    border-radius: var(--border-radius)
+
+    &:hover,
+    &:focus
+      background-color: var(--navigation-item__bg--hover, var(--color-neutral, #fbfbfb))
+      color: var(--navigation-item__color--hover, var(--color-accent, #ee1696))
+.menu
+  list-style: none
+  &Icon
+    font-size: var(--font-size-huge)
+</style>
